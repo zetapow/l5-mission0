514d0f5442a7ee0d36fdee546f27318c3edba82d
Peter Zhou <zetapow@gmail.com>
2025-05-07 10:17:34 +1200

    feat: install and test tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<h1 class="text-2xl font-bold hover:underline">Hello World</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<style lang="postcss">
+   @reference "tailwindcss";
+
+   :global(html) {
+      background-color: theme(--color-gray-300);
+   }
+</style>
+
+<svelte:head>
+   <title>Marketing Association</title>
+</svelte:head>
